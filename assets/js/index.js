bb06b264032c833f1f50105ade375d0642a59939
João Carmassi <joaovitorcarmassi@gmail.com>
2024-07-04 00:49:44 -0300

    0.01v

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,13 @@
 function detectaScroll() {
   window.addEventListener("scroll", function () {
     const nav = document.getElementById("container-nav");
+    const html = document.querySelector("html");
     if (window.scrollY > 0) {
+      html.setAttribute("data-contexto", "white");
       nav.setAttribute("theme", "white");
     } else {
       nav.setAttribute("theme", "black");
+      html.setAttribute("data-contexto", "black");
     }
   });
 }
