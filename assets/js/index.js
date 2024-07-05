b80f0848c2c926875b6e3ea0f77696cee52a921a
João Carmassi <joaovitorcarmassi@gmail.com>
2024-07-04 21:38:43 -0300

    trocar img fundo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,12 @@
 function detectaScroll() {
   window.addEventListener("scroll", function () {
-    const nav = document.getElementById("container-nav");
     const iconNav = document.getElementById("iconNav");
-
     const html = document.querySelector("html");
+
     if (window.scrollY > 0) {
       html.setAttribute("data-contexto", "white");
-      nav.setAttribute("theme", "white");
       iconNav.setAttribute("src", "./assets/imgs/icon-preto.png");
     } else {
-      nav.setAttribute("theme", "black");
       html.setAttribute("data-contexto", "black");
       iconNav.setAttribute("src", "./assets/imgs/icon-branco.png");
     }
