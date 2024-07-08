694f4149c62bbc6d23ec537d5a849633d935e007
João Carmassi <joaovitorcarmassi@gmail.com>
2024-07-07 22:25:40 -0300

    alterando nomes objeto projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,27 +5,27 @@ const imgsProjetos = document.querySelectorAll(".img-projetos");
 const projetos = [
   (projeto = {
     nome: "Alura Book",
-    url: "Alura-Book",
+    imagem: "Alura-Book",
     descricao: "Página de busca dos livros.",
-    site: "https://joao-carmassi.github.io/Alura-Books/pagina%201/index.html",
+    link: "https://joao-carmassi.github.io/Alura-Books/pagina%201/index.html",
   }),
   (projeto = {
     nome: "Calmaria Spa",
-    url: "Calmaria-Spa",
+    imagem: "Calmaria-Spa",
     descricao: "Site de um Spa",
-    site: "https://joao-carmassi.github.io/Calmaria-Spa/",
+    link: "https://joao-carmassi.github.io/Calmaria-Spa/",
   }),
   (projeto = {
     nome: "D&D",
-    url: "RPG",
+    imagem: "RPG",
     descricao: "Site que simula combate de personagens de RPG.",
-    site: "https://joao-carmassi.github.io/Cartas-Personagens/",
+    link: "https://joao-carmassi.github.io/Cartas-Personagens/",
   }),
   (projeto = {
     nome: "Lista de Compras",
-    url: "Lista-de-Compras",
+    imagem: "Lista-de-Compras",
     descricao: "Página de uma lista de compras.",
-    site: "https://joao-carmassi.github.io/Lista-de-compras/",
+    link: "https://joao-carmassi.github.io/Lista-de-compras/",
   }),
 ];
 // "Alura-Book", "Calmaria-Spa", "RPG", "Lista-de-Compras";
@@ -35,10 +35,10 @@ adicionaDadosProjetos();
 function adicionaDadosProjetos() {
   let index = 0;
   imgsProjetos.forEach((element) => {
-    element.style.backgroundImage = `url('./assets/imgs/projetos/${projetos[index].url}.png')`;
+    element.style.backgroundImage = `url('./assets/imgs/projetos/${projetos[index].imagem}.png')`;
     element.querySelector("h2").textContent = projetos[index].nome;
     element.querySelector("p").textContent = projetos[index].descricao;
-    element.setAttribute("href", `${projetos[index].site}`);
+    element.setAttribute("href", `${projetos[index].link}`);
     index++;
   });
 }
