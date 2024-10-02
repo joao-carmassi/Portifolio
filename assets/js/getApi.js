65302f5fe814ce070fa995e5d265ce2505d64f02
João Vitor Carmassi <90992816+joao-carmassi@users.noreply.github.com>
2024-10-02 16:09:14 -0300

    Update getApi.js

--- assets/js/getApi.js
+++ assets/js/getApi.js
@@ -2,7 +2,11 @@ const ApiLink = "https://yeti-balanced-longhorn.ngrok-free.app/data/mensagens";
 
 async function getApi() {
   try {
-    const response = await fetch(ApiLink);
+    const response = await fetch(ApiLink, {
+      headers: {
+      "ngrok-skip-browser-warning": "true",
+    },
+    });
     const data = await response.json();
     return data;
   } catch (error) {
@@ -15,6 +19,7 @@ async function postApi(nome, mensagem, dia, id) {
     const response = await fetch(ApiLink, {
       method: "POST",
       headers: {
+        "ngrok-skip-browser-warning": "true",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ nome: nome, mensagem: mensagem, dia: dia, id }),
